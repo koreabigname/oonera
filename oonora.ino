09c2dfa0f70d089ae694e7a3b882e9ace2de6e95
Jingun Jung <koreabigname@gmail.com>
2016-10-17 23:15:00 +0900

    added function left&right sync

--- oonora.ino
+++ oonora.ino
@@ -4,6 +4,10 @@
 #define PIN_LEFT_MOTOR_IN1    7
 #define PIN_LEFT_MOTOR_IN2    8
 #define PIN_LEFT_MOTOR_VREF   9
+#define PIN_RIGHT_LED_RED     10
+#define PIN_RIGHT_LED_YELLOW  11
+#define PIN_LEFT_LED_RED      12
+#define PIN_LEFT_LED_YELLOW   13
 
 char inputSerialBuffer[10];
 char incomingSerialData;
@@ -15,6 +19,10 @@ void setup() {
   pinMode(PIN_RIGHT_MOTOR_IN2, OUTPUT);
   pinMode(PIN_LEFT_MOTOR_IN1, OUTPUT);
   pinMode(PIN_LEFT_MOTOR_IN2, OUTPUT);
+  pinMode(PIN_RIGHT_LED_RED, OUTPUT);
+  pinMode(PIN_RIGHT_LED_YELLOW, OUTPUT);
+  pinMode(PIN_LEFT_LED_RED, OUTPUT);
+  pinMode(PIN_LEFT_LED_YELLOW, OUTPUT);
 }
 
 void loop() {
@@ -22,13 +30,35 @@ void loop() {
     incomingSerialData = Serial.read();
     switch(incomingSerialData) {
       case 'L':  // Left motor
+        digitalWrite(PIN_LEFT_LED_RED, HIGH);
         outputPulseToMotor(PIN_LEFT_MOTOR_IN1, PIN_LEFT_MOTOR_IN2, PIN_LEFT_MOTOR_VREF);
+        Serial.write("Ack");
         break;
       case 'R':  // Right motor
+        digitalWrite(PIN_RIGHT_LED_RED, HIGH);
         outputPulseToMotor(PIN_RIGHT_MOTOR_IN1, PIN_RIGHT_MOTOR_IN2, PIN_RIGHT_MOTOR_VREF);
+        Serial.write("Ack");
+        break;
+      case 'A':  // Left and Right motor
+        digitalWrite(PIN_LEFT_LED_RED, HIGH);
+        digitalWrite(PIN_LEFT_LED_YELLOW, HIGH);
+        digitalWrite(PIN_RIGHT_LED_RED, HIGH);
+        digitalWrite(PIN_RIGHT_LED_YELLOW, HIGH);
+        outputPulseToMotor(PIN_LEFT_MOTOR_IN1, PIN_LEFT_MOTOR_IN2, PIN_LEFT_MOTOR_VREF);
+        outputPulseToMotor(PIN_RIGHT_MOTOR_IN1, PIN_RIGHT_MOTOR_IN2, PIN_RIGHT_MOTOR_VREF);
+        Serial.write("Ack");
         break;
     }
+  } else {
+    digitalWrite(PIN_LEFT_LED_YELLOW, HIGH);
+    digitalWrite(PIN_RIGHT_LED_YELLOW, HIGH);
   }
+  delay(100);
+  digitalWrite(PIN_LEFT_LED_RED, LOW);
+  digitalWrite(PIN_LEFT_LED_YELLOW, LOW);
+  digitalWrite(PIN_RIGHT_LED_RED, LOW);
+  digitalWrite(PIN_RIGHT_LED_YELLOW, LOW);
+  delay(100);  
 }
 
 int outputPulseToMotor( int pin_in1, int pin_in2, int pin_vref ) {
@@ -50,7 +80,6 @@ int outputPulseToMotor( int pin_in1, int pin_in2, int pin_vref ) {
       analogWrite(pin_vref, map(mapOutputValue, 150, 255, 80, 255));
       break;
   }
-  Serial.write("Ack");
 }
 
 int getDirection( int inputValue ) {
